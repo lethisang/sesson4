80bba34dd9d66d55d429e232106627d51ea9d1f7
lethisang <133087444+lethisang@users.noreply.github.com>
2023-06-01 00:31:13 +0700

    Update index3.php

--- index3.php
+++ index3.php
@@ -1,71 +1,69 @@
-<?php
-//cau3. Kiểm tra năm nay là năm chẵn hay năm lẻ, in ra màn hình kết quả chẵn hay lẻ.
-function checkEvenOrOddYear() {
-    $currentYear = date('Y');
-
-    if ($currentYear % 2 == 0) {
-        return "năm chẵn";
-    } else {
-        return "năm lẻ";
-    }
-}
-
-// Kiểm tra năm hiện tại là năm chẵn hay năm lẻ
-$result = checkEvenOrOddYear();
-echo "Năm hiện tại là $result.";
+<?php 
+// Bài 1: Viết chương trình PHP, sử dụng câu lệnh điều kiện if else để kiểm tra 1 số là số chẵn hay số lẻ?
+	$number = 2;
+	if ($number % 2 == 0) {
+		echo "1. Kết quả: $number là số chẵn.";
+	} else {
+		echo "1. Kết quả: $number là số lẻ.";	
+	}
 ?>
-</br>
-<?php
-//cau4. Viết chương trình PHP, sử dụng câu lệnh vòng lặp For để in ra số từ 1 đến 100.
-function printNumbers() {
-    for ($i = 1; $i <= 100; $i++) {
-        echo $i . " ";
-    }
-}
 
-// Gọi hàm để in ra các số từ 1 đến 100
-printNumbers();
+<?php 
+// Bài 2: Viết chương trình PHP, sử dụng câu lệnh if else để xếp hạng học lực của học sinh dựa trên điểm thi giữa kỳ, điểm thi cuối kỳ.
+	$diemGiuaKy = 7.5;
+	$diemCuoiKy = 8;
+	$DiemTB = ($diemGiuaKy * 0.3) + ($diemCuoiKy * 0.7);
+
+	echo "<br> <br> 2. Kết quả: ";
+	if ($DiemTB >= 9.0) {
+		echo "Xuất sắc.";
+	} elseif ($DiemTB >= 7.0 && $DiemTB < 9.0) {
+		echo "Giỏi.";
+	} elseif ($DiemTB >= 5.0 && $DiemTB < 7.0) {
+		echo "Khá.";
+	} elseif ($DiemTB < 5.0) {
+		echo "Yếu.";
+	}
 ?>
-</br>
-<!DOCTYPE html>
-<html>
-<head>
-    <style>
-        .even {
-            font-weight: bold;
-        }
-    </style>
-</head>
-<body>
-    <?php
-    /*câu 5. Viết trang PHP hiển thị dãy số từ 1 đến 100 sao cho số chẵn là chữ in đậm, số lẻ là chữ in thường.
-    Kết quả: 1 2 3 4….., 100 .Hướng dẫn: Sử dụng vòng lặp for, 1 biến đếm i, toán tử %.*/
-    function printNumbers() {
-        for ($i = 1; $i <= 100; $i++) {
-            if ($i % 2 == 0) {
-                echo "<span class='even'>$i </span>";
-            } else {
-                echo "$i ";
-            }
-        }
-    }
 
-    // Gọi hàm để in ra dãy số từ 1 đến 100
-    printNumbers();
-    ?>
-</body>
-</html>
 <?php
-/* câu 6. Viết chương trình PHP, sử dụng vòng lặp For each in ra các năm trong mảng có sẵn dưới đây:
-$nam = array(1990, 1991, 1992, 1993, 1994, 1995);*/
-function printYears($array) {
-    foreach ($array as $year) {
-        echo $year . " ";
-    }
-}
+// Bài 3: Kiểm tra năm nay là năm chẵn hay năm lẻ, in ra màn hình chẵn hay lẻ.
+	$curentYear = date("Y");
 
-$nam = array(1990, 1991, 1992, 1993, 1994, 1995);
+	echo "<br> <br> 3. Kết quả: ";
+	if ($curentYear % 2 == 0) {
+		echo "Năm nay là năm chẵn.";
+	} else {
+		echo "Năm nay là năm lẻ.";
+	}
+?>
 
-echo "Các năm trong mảng: ";
-printYears($nam);
+<?php 
+// Bài 4: Viết chương trình PHP, sử dụng câu lệnh vòng lặp For để in ra số từ 1 đến 100
+	echo "<br> <br> 4.Kết quả: ";
+	for ($i = 1; $i <= 100; $i++) { 
+		echo $i . " ";
+	}
+?>
+
+<?php 
+// Bài 5: Viết trang PHP hiển thị dãy số từ 1 đến 100 sao cho số chẵn là chữ in đậm, số lẻ là chữ in thường.
+	echo "<br> <br> 5.Kết quả: ";
+	for ($i = 1; $i <= 100; $i++) { 
+		if ($i % 2 == 0) {
+			echo "<b>$i</b> ";
+		} else {
+			echo "$i ";
+		}
+	}
+?>
+
+<?php 
+// Bài 6: Viết chương trình PHP, sử dụng vòng lăpj For each in ra các năm trong mảng có sẵn dưới đây: $nam = array(1990, 1991, 1992, 1993, 1994, 1995)
+	echo "<br> <br> 6.Kết quả: ";
+	$nam = array(1990, 1991, 1992, 1993, 1994, 1995);
+
+	foreach($nam as $value) {
+		echo "$value ";
+	}
 ?>
